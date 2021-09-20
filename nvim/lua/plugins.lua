71449ee59075a811c8aedcc111f8183ace02e12b
Andrew Cline <andrew.cline77@gmail.com>
2021-09-19 21:54:23 -0500

    feat: replace ultisnip with luasnip

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -73,24 +73,26 @@ return require('packer').startup(function()
     end
   }
 
-  use 'sirver/ultisnips'
+  use {
+    'L3MON4D3/LuaSnip',
+  }
 
   use {
     'hrsh7th/nvim-compe',
     config = function()
       require('compe').setup { 
-        enabled = true;
-        autocomplete = true;
-        debug = false;
-        min_length = 1;
-        preselect = 'enable';
-        throttle_time = 80;
-        source_timeout = 200;
-        resolve_timeout = 800;
-        incomplete_delay = 400;
-        max_abbr_width = 100;
-        max_kind_width = 100;
-        max_menu_width = 100;
+        enabled = true,
+        autocomplete = true,
+        debug = false,
+        min_length = 1,
+        preselect = 'enable',
+        throttle_time = 80,
+        source_timeout = 200,
+        resolve_timeout = 800,
+        incomplete_delay = 400,
+        max_abbr_width = 100,
+        max_kind_width = 100,
+        max_menu_width = 100,
         documentation = {
           border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
           winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
@@ -98,16 +100,16 @@ return require('packer').startup(function()
           min_width = 60,
           max_height = math.floor(vim.o.lines * 0.3),
           min_height = 1,
-        };
+        },
 
         source = {
-          path = true;
-          buffer = true;
-          calc = true;
-          nvim_lsp = true;
-          nvim_lua = true;
-          ultisnips = true;
-        };
+          path = true,
+          buffer = true,
+          calc = true,
+          nvim_lsp = true,
+          nvim_lua = true,
+          lua_snip = true,
+        },
       }
     end
     }
