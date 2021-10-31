508dbd0b6d995e6f78d78c3b2eaabc22943f7e3a
Andrew Cline <andrew.cline77@gmail.com>
2021-10-30 23:36:42 -0500

    feat: work on wezterm fonts

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,13 @@
 local wezterm = require("wezterm")
 
 return {
-  font = wezterm.font('FiraCode Nerd Font Mono'),
+  font = wezterm.font_with_fallback({
+    "Dank Mono",
+    "SauceCodePro Nerd Font",
+  }),
+  font_size = 14,
   color_scheme = "ayu",
+  line_height = 1.1,
   enable_tab_bar = false,
   window_padding = {
     left = 20,
