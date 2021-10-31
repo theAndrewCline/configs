6a9e3ca8605d491b96d2e2f1e8482f90cc483bf7
Andrew Cline <andrew.cline77@gmail.com>
2021-10-30 20:53:13 -0500

    feat: update tmux and wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+
+return {
+  font = wezterm.font('FiraCode Nerd Font Mono'),
+  color_scheme = "ayu",
+  enable_tab_bar = false,
+  window_padding = {
+    left = 20,
+    right = 20,
+    top = 20,
+    bottom = 10
+  }
+}
