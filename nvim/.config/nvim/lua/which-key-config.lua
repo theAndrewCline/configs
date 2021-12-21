095aaa880f4f8b2cdcc3372502453b32c6678850
Andrew Cline <andrew.cline77@gmail.com>
2021-12-21 07:16:09 -0600

    fix: code action mapping

--- nvim/.config/nvim/lua/which-key-config.lua
+++ nvim/.config/nvim/lua/which-key-config.lua
@@ -3,7 +3,7 @@ local wk = require("which-key")
 wk.register({
   p = { ':Telescope git_files<cr>', 'Search Files' },
   [";"] = { ":Telescope buffers<cr>", "List Buffers" },
-  a = { ':Lspsaga code_action<cr>', 'Code Action' },
+  a = { ':lua vim.lsp.buf.code_action()<CR>', 'Code Action' },
   s = { ':Telescope live_grep<cr>', 'Search' },
   S = { ':PackerSync<cr>', 'Update Plugins' },
   t = { ':Telescope<cr>', 'Telescope' },
