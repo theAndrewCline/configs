60f34b646a019a24acf4a88088229c739cce778f
Andrew Cline <acline@precisionplanting.com>
2022-06-07 11:24:37 -0500

    feat: add abbr for aws config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,23 +1,10 @@
 fish_vi_key_bindings
 
-set EDITOR nvim
-
-fish_add_path /opt/homebrew/bin
-fish_add_path ~/.local/bin
-
-fish_add_path ~/.local/flutter/bin
-fish_add_path ~/.scripts
-fish_add_path ~/go/bin
-fish_add_path /usr/local/opt/php@7.4/bin
-fish_add_path /usr/local/opt/php@7.4/sbin
-
-set FZF_DEFAULT_COMMAND "rg --files"
-
 alias luamake=/home/andrewcline/.config/nvim/lua-language-server/3rd/luamake/luamake
 
-fish_add_path ~/.yarn/bin
-fish_add_path ~/.config/yarn/global/node_modules/.bin
-fish_add_path ~/.scripts
+set -xg EDITOR nvim
 
 abbr -a -g ef $EDITOR ~/.config/fish/config.fish
 abbr -a -g sf source ~/.config/fish/config.fish
+abbr -a -g envim $EDITOR ~/.config/nvim/init.lua
+abbr -a -g eaws $EDITOR ~/.aws
