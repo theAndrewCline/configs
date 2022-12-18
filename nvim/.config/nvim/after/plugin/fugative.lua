97f33fe5a55b7d24213d2b68e4089aba6773d3de
Andrew Cline <acline@precisionplanting.com>
2022-12-18 07:54:38 -0600

    feat: add basic fugative commands

---
+++ nvim/.config/nvim/after/plugin/fugative.lua
@@ -0,0 +1,13 @@
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[G]it [S]tatus' })
+
+vim.keymap.set('n', '<leader>gp',
+  function ()
+    vim.cmd.Git({ args = { "pull" } })
+  end,
+  { desc = '[G]it [P]ull' })
+
+vim.keymap.set('n', '<leader>gP',
+  function ()
+    vim.cmd.Git({ args = { "push" } })
+  end,
+  { desc = '[G]it [P]ush' })
